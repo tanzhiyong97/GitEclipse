4fb3eed8b5c04bdc67c8033a851479f4578706ca
tanzhiyong97 <tanzhiyong97@gmail.com>
2019-12-06 13:48:48 +0800

    hashset

---
+++ src/collection接口和iterator接口/YoungCollection.java
@@ -0,0 +1,25 @@
+package collection接口和iterator接口;
+
+import java.util.*;
+public class YoungCollection {
+	public static void main(String[] args) {
+		try {
+			ArrayList c=new ArrayList();
+			c.add("美美"); //添加元素
+			c.add(6);
+			System.out.println("集合C的元素个数为："+c.size());
+			c.remove(6);
+			System.out.println("集合C的元素个数为："+c.size());
+			//判断是否包含指定字符
+			System.out.println("集合C的元素是否包含美美字符串"+c.contains("美美"));
+			System.out.println("集合C的元素是否包含美美字符串"+c.contains("美美"));
+			c.add("江湖");
+			System.out.println("集合C的元素："+c);
+		}
+		catch(Exception e) {
+			System.out.println("输出错误:"+e);
+			e.printStackTrace();
+		}
+	}
+
+}
