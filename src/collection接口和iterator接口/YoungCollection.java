ddce8f8614ef92587d61edfa0cb6c75c4f9969e9
tanzhiyong97 <tanzhiyong97@gmail.com>
2019-12-06 14:22:59 +0800

    delete the try/catch

--- src/collection接口和iterator接口/YoungCollection.java
+++ src/collection接口和iterator接口/YoungCollection.java
@@ -3,7 +3,6 @@ package collection接口和iterator接口;
 import java.util.*;
 public class YoungCollection {
 	public static void main(String[] args) {
-		try {
 			ArrayList c=new ArrayList();
 			c.add("美美"); //添加元素
 			c.add(6);
@@ -15,13 +14,7 @@ public class YoungCollection {
 			System.out.println("集合C的元素是否包含美美字符串"+c.contains("美美"));
 			c.add("江湖");
 			System.out.println("集合C的元素："+c);
-			System.out.println("test");
-			System.out.println("用来测试代码的对比");
-		}
-		catch(Exception e) {
-			System.out.println("输出错误:"+e);
-			e.printStackTrace();
-		}
+
 	}
 
 }
